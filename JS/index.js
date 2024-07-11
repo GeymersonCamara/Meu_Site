eb3cc9cd72de9dbfe42f768bd49062829e1bdb8d
Geymerson Câmara <geymersonsuporte@gmail.com>
2024-07-11 10:07:22 -0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -39,3 +39,17 @@ const meuObservador2 = new IntersectionObserver((entrada) => {
 const elemento2 = document.querySelectorAll(".photo");
 
 elemento2.forEach((elemento2) => meuObservador2.observe(elemento2));
+
+const meuObservador3 = new IntersectionObserver((entrada) => {
+    entrada.forEach((entrar) => {
+        if (entrar.isIntersecting) {
+            entrar.target.classList.add('aparecer2')
+        } else {
+            entrar.target.classList.remove('aparecer2')
+        }
+    });
+});
+
+const elemento3 = document.querySelectorAll(".photo2");
+
+elemento3.forEach((elemento3) => meuObservador3.observe(elemento3));
